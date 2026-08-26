e208cdd399cb55d361c95e898d4fa60a0a9bb7ac
Abihshek Karmakar <abhi.karmakar604@gmail.com>
2026-08-26 13:51:36 +0530

    Finished Module 18 training

---
+++ Module18 - Machine Learning I/Lesson 6 – Implementation of linear regression/a1-multiple-linear-regression.py
@@ -0,0 +1,35 @@
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+dataset = pd.read_csv(r'petrol_consumption.csv')
+
+dataset.head()
+
+dataset.describe()
+
+X = dataset[['Petrol_tax', 'Average_income', 'Paved_Highways',
+             'Population_Driver_licence(%)']]
+y = dataset['Petrol_Consumption']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=['Coefficient'])
+coeff_df
+
+y_pred = regressor.predict(X_test)
+df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+df
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(
+    metrics.mean_squared_error(y_test, y_pred)))
